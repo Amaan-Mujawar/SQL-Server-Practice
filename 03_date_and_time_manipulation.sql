826a8b4aa91d28a68e3cab35f0794180e4e7556d
Amaan Mujawar <amaanmujawar175@gmail.com>
2026-07-02 16:37:48 +0530

    feat: add date/time retrieval, parsing, and formatting scripts

---
+++ 03_date_and_time_manipulation.sql
@@ -0,0 +1,82 @@
+SELECT CURRENT_TIMEZONE() AS TimezoneName;
+
+SELECT CURRENT_TIMEZONE_ID() AS TimezoneID;
+
+SELECT SYSDATETIMEOFFSET() AS ServerTimeWithOffset;
+
+SELECT * FROM sys.time_zone_info;
+
+SELECT GETDATE() AS CurrentDateTime;
+
+SELECT CURRENT_DATE;
+
+SELECT CURRENT_TIMESTAMP;
+
+SELECT SYSDATETIME();
+
+SELECT CAST(GETDATE() AS TIME);
+
+SELECT * FROM employee_data;
+
+SELECT MONTH(joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATENAME(MONTH, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATEPART(MONTH, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT FORMAT(joining_date, 'MM') AS joining_month, joining_date
+FROM employee_data;
+
+SELECT YEAR(joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATENAME(YEAR, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATEPART(YEAR, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT FORMAT(joining_date, 'yy') AS joining_month, joining_date
+FROM employee_data;
+
+SELECT FORMAT(joining_date, 'yyyy') AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DAY(joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATENAME(DAY, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATEPART(DAY, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT FORMAT(joining_date, 'dd') AS joining_month, joining_date
+FROM employee_data;
+
+SELECT FORMAT(joining_date, 'ddd') AS joining_month, joining_date
+FROM employee_data;
+
+SELECT FORMAT(joining_date, 'dddd') AS joining_month, joining_date
+FROM employee_data;
+
+SELECT YEAR(joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATENAME(DAYOFYEAR, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATENAME(WEEKDAY, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATEPART(DAYOFYEAR, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DATEPART(WEEKDAY, joining_date) AS joining_month, joining_date
+FROM employee_data;
+
+SELECT DAY(joining_date) AS joining_month, joining_date
+FROM employee_data;

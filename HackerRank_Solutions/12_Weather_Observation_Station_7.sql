b4852fe28c0b2b9e547a7173348dec643a2e975e
Amaan Mujawar <amaanmujawar175@gmail.com>
2026-07-06 00:58:13 +0530

    feat: add HackerRank solution for Weather Observation Station 7

---
+++ HackerRank_Solutions/12_Weather_Observation_Station_7.sql
@@ -0,0 +1,18 @@
+/*
+Question: Weather Observation Station 7
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+The STATION table is described as follows:
+        STATION
+Field           Type
+ID              NUMBER
+CITY            VARCHAR2(21)
+STATE           VARCHAR2(2)
+LAT_N           NUMBER
+LONG_W          NUMBER
+*/
+
+-- Answer:
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%[aeiou]';

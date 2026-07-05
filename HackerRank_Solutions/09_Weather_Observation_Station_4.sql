2975a045f002acb2bacba5f001a0f882f7dc9e7b
Amaan Mujawar <amaanmujawar175@gmail.com>
2026-07-05 12:38:56 +0530

    feat: add HackerRank solution for Weather Observation Station 4

---
+++ HackerRank_Solutions/09_Weather_Observation_Station_4.sql
@@ -0,0 +1,17 @@
+/*
+Question: Weather Observation Station 4
+Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+
+The STATION table is described as follows:
+        STATION
+Field           Type
+ID              NUMBER
+CITY            VARCHAR2(21)
+STATE           VARCHAR2(2)
+LAT_N           NUMBER
+LONG_W          NUMBER
+*/
+
+-- Answer:
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) 
+FROM STATION;

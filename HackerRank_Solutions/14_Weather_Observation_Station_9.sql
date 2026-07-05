c6ca4cd6d845690704c96d91276ee5b8fc5fe434
Amaan Mujawar <amaanmujawar175@gmail.com>
2026-07-06 00:58:40 +0530

    feat: add HackerRank solution for Weather Observation Station 9

---
+++ HackerRank_Solutions/14_Weather_Observation_Station_9.sql
@@ -0,0 +1,25 @@
+/*
+Question: Weather Observation Station 9
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+The STATION table is described as follows:
+        STATION
+Field           Type
+ID              NUMBER
+CITY            VARCHAR2(21)
+STATE           VARCHAR2(2)
+LAT_N           NUMBER
+LONG_W          NUMBER
+*/
+
+-- Answer (Method 1: Using NOT LIKE - Clean and Intuitive):
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE '[aeiou]%';
+
+/*
+-- Method 2: Using the Negation Caret (^) inside Character Class
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '[^aeiou]%';
+*/
